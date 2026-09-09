0441342a8e9c85668b8963d4017f66b2f9dabc92
daniely998 <danzone998@gmail.com>
2026-09-09 17:02:45 +0800

    priority queue

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -9,21 +9,39 @@ public class PriorityQueueTests
     // Scenario: 
     // Expected Result: 
     // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    public void TestPriorityQueue_RemoveHighestPriorityItem()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var fruits = new PriorityQueue();
+        fruits.Enqueue("Apple", 2);
+        fruits.Enqueue("Orange", 1);
+        fruits.Enqueue("Banana", 3);
+
+        var highest = fruits.Dequeue();
+        Assert.AreEqual("Banana", highest);
     }
 
     [TestMethod]
     // Scenario: 
     // Expected Result: 
     // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    public void TestPriorityQueue_MultipleHighestPriorityItems()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var fruits = new PriorityQueue();
+        fruits.Enqueue("Apple", 2);
+        fruits.Enqueue("Grapes", 3);
+        fruits.Enqueue("Banana", 3);
+        fruits.Enqueue("Orange", 1);
+
+        var removed = fruits.Dequeue();
+        Assert.AreEqual("Grapes", removed);
     }
 
     // Add more test cases as needed below.
+    [TestMethod]
+    public void TestPriorityQueue_EmptyQueue()
+    {
+        var fruits = new PriorityQueue();
+        var e = Assert.ThrowsException<InvalidOperationException>(() => fruits.Dequeue());
+        Assert.AreEqual("The queue is empty.", e.Message);
+    }
 }
